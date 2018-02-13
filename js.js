d7a25c7b2780a7315951ffbcc041370a5067ca4b
Caitlienne Diane Juan <caitliennedianejuan@gmail.com>
2018-02-14 07:58:58 +0800

    updated login with js

--- js.js
+++ js.js
@@ -5,3 +5,20 @@ function closeSidebar() {
 function openSidebar() {
     document.getElementById("sidebarjs").style.display = "block";
 }
+
+function checkForm() {
+    var usernameString = document.forms["loginform"]["username"].value;
+    var passwordString = document.forms["loginform"]["password"].value;
+    
+    if(usernameString === "Ugandan Knuckles"){
+        if(passwordString === "admin123"){
+            window.location = "member_home.html";
+            return false;
+        }
+    }
+    
+    else {
+        alert("Incorrect username and/or password");
+        return false;
+    }
+}
